9ea00ce0559cc3ade660a658ee8a3796a7e78312
shawnxjf1 <shawn_angel@qq.com>
2016-09-22 20:15:03 +0800

    2016年9月22日

--- src/main/java/com/person/jvm/ClassLoarderTest.java
+++ src/main/java/com/person/jvm/ClassLoarderTest.java
@@ -1,5 +1,11 @@
 package com.person.jvm;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+import org.junit.Test;
+
 /**
  * 背景： 学习ClassLoader.getResource()时候不知道classLoader的边界，所以有必要把java类加载机制弄清楚
  * 
@@ -9,5 +15,73 @@ package com.person.jvm;
  *
  */
 public class ClassLoarderTest {
+
+	/**
+	 * 加载过程为：
+	 * 
+	 * 命令的时候，JVM会将HelloWorld.class加载到内存中，并形成一个Class的对象HelloWorld.class。
+	 * 其中的过程就是类加载过程： 1、寻找jre目录，寻找jvm.dll，并初始化JVM； 2、产生一个Bootstrap
+	 * Loader（启动类加载器）； 3、Bootstrap Loader自动加载Extended
+	 * Loader（标准扩展类加载器），并将其父Loader设为Bootstrap Loader。 4、Bootstrap
+	 * Loader自动加载AppClass Loader（系统类加载器），并将其父Loader设为Extended Loader。
+	 * 5、最后由AppClass Loader加载HelloWorld类。
+	 */
+	/**
+	 * 二、类加载器各自搜索的目录 为了弄清楚这个问题，首先还要看看System类的API doc文档。
+	 * 
+	 * 1、Bootstrap
+	 * Loader（启动类加载器）：加载System.getProperty("sun.boot.class.path")所指定的路径或jar。
+	 * 2、Extended
+	 * Loader（标准扩展类加载器ExtClassLoader）：加载System.getProperty("java.ext.dirs")所指定的路径或jar。在使用Java运行程序时，也可以指定其搜索路径，例如：java
+	 * -Djava.ext.dirs=d:/projects/testproj/classes HelloWorld 3、AppClass
+	 * Loader（系统类加载器AppClassLoader）：加载System.getProperty("java.class.path")所指定的路径或jar。在使用Java运行程序时，也可以加上-cp来覆盖原有的Classpath设置，例如：
+	 * java -cp ./lavasoft/classes HelloWorld
+	 * ExtClassLoader和AppClassLoader在JVM启动后，会在JVM中保存一份，并且在程序运行中无法改变其搜索路径。如果想在运行时从其他搜索路径加载类，就要产生新的类加载器。
+	 * 
+	 * @param args
+	 */
+	@Test
+	public void testClassLoaderLevel() {
+		ClassLoarderTest hello = new ClassLoarderTest();
+		Class c = hello.getClass();
+		ClassLoader loader = c.getClassLoader();
+		System.out.println("classLoader" + loader);
+		System.out.println("classLoader.getParent()=" + loader.getParent());
+		System.out.println("classLoader.getParent().getParent()=" + loader.getParent().getParent());
+
+		// classLoadersun.misc.Launcher$AppClassLoader@73d16e93
+		// classLoader.getParent()=sun.misc.Launcher$ExtClassLoader@15db9742
+		// classLoader.getParent().getParent()=null
+		System.out.println("appclassLoader的加载器为：" + loader.getClass().getClassLoader());
+
+		System.out.println("appclassLoader的loader.getSystemClassLoader()为：" + loader.getSystemClassLoader());
+		// 系统类加载器为appClassLoader:appclassLoader的loader.getSystemClassLoader()为：sun.misc.Launcher$AppClassLoader@37a71e93
+
+	}
+
+	@Test
+	public void testLoaderStyle() {
+		-- 参考：http://blog.csdn.net/wan368500/article/details/8215668
+		---运行失败 --需要整理
+		try {
+			URL url;
+			url = new URL("./");
+			ClassLoader myloader = new URLClassLoader(new URL[] { url });
+			Class c;
+			c = myloader.loadClass("test.Test3");
+			TestClassLoder t3 = (TestClassLoder) c.newInstance();
+
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException | IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MalformedURLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+	}
 
 }
