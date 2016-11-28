304ea5dc70340124a5d6855ea7748083d7f2e0d0
shawnxjf1 <shawn_angel@qq.com>
2016-11-29 00:07:28 +0800

    2016-11-29

--- gitPullCommitPush.sh
+++ gitPullCommitPush.sh
@@ -2,7 +2,8 @@ curDir=./
 echo begin git add $curDir
 git add $curDir
 echo begin git pull $curDir
-git pull $curDir
+##git pull $curDir
+git pull
 ##date 命令赋值使用$()
 comment=$(date "+%Y-%m-%d")
 echo begin commit -m $comment
